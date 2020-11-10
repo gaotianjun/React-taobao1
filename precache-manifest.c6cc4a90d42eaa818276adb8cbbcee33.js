6b1a134de276b3217f40f23f4558b9db0890b80d
gaotianjun <44943100+gaotianjun@users.noreply.github.com>
2020-11-10 08:16:53 +0800

    Add files via upload
    taobao

---
+++ precache-manifest.c6cc4a90d42eaa818276adb8cbbcee33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a8846d17ebecffa668589ed0c62ac5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85d2f98921243b59fac6",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "8f198c1fa3ca5461da45",
+    "url": "/static/css/main.a3d27aa7.chunk.css"
+  },
+  {
+    "revision": "85d2f98921243b59fac6",
+    "url": "/static/js/2.4f0036f3.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.4f0036f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f198c1fa3ca5461da45",
+    "url": "/static/js/main.e0fcd7c8.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
